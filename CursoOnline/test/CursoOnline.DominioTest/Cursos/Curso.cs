d4ae670bca52ed00d60e6fe986b63aa7e35d1e56
Guilherme Assi Pereira <guilherme.pereira@havan.com.br>
2022-03-04 15:57:21 -0300

    Inicio do curso com testes básicos e utilizando Expected Objects

---
+++ CursoOnline/test/CursoOnline.DominioTest/Cursos/Curso.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace CursoOnline.DominioTest.Cursos
+{
+    public class Curso
+    {
+        public Curso(string nome, double cargaHoraria, PublicoAlvoEnum publicoAlvo, decimal valor)
+        {
+            Nome = nome;
+            CargaHoraria = cargaHoraria;
+            PublicoAlvo = publicoAlvo;
+            Valor = valor;
+        }
+
+        public string Nome { get; private set; }
+        public double CargaHoraria { get; private set; }
+        public PublicoAlvoEnum PublicoAlvo { get; private set; }
+        public decimal Valor { get; private set; }
+    }
+
+    public enum PublicoAlvoEnum
+    {
+        Estudante,
+        Universitario,
+        Empregado,
+        Empreendedor
+    }
+}
---
+++ CursoOnline/test/CursoOnline.DominioTest/Cursos/CursoTest.cs
@@ -0,0 +1,46 @@
+﻿using ExpectedObjects;
+using Xunit;
+
+namespace CursoOnline.DominioTest.Cursos
+{
+    public class CursoTest
+    {
+        [Fact]
+        public void CriarCurso()
+        {
+            //Arrange
+            const string nome = "TI";
+            const double cargaHoraria = 80;
+            const PublicoAlvoEnum publicoAlvo = PublicoAlvoEnum.Estudante;
+            const decimal valor = 950;
+
+            //Act
+            var curso = new Curso(nome, cargaHoraria, publicoAlvo, valor);
+
+            //Assert
+            Assert.Equal(nome, curso.Nome);
+            Assert.Equal(cargaHoraria, curso.CargaHoraria);
+            Assert.Equal(publicoAlvo, curso.PublicoAlvo);
+            Assert.Equal(valor, curso.Valor);
+        }
+
+        [Fact]
+        public void CriarCursoComExpected()
+        {
+            //Arrange
+            var cursoEsperado = new
+            {
+                Nome = "TI",
+                CargaHoraria = (double)80,
+                PublicoAlvo = PublicoAlvoEnum.Universitario,
+                Valor = (decimal)950
+            };
+
+            //Act
+            var curso = new Curso(cursoEsperado.Nome, cursoEsperado.CargaHoraria, cursoEsperado.PublicoAlvo, cursoEsperado.Valor);
+
+            //Assert
+            cursoEsperado.ToExpectedObject().ShouldMatch(curso);
+        }
+    }
+}
